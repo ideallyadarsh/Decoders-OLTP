f558df765c0e1719ea38bcaa6aaea28df1332c3d
Sumit <sumit.kr445@gmail.com>
2020-08-23 22:01:30 +0530

    added Question model

---
+++ lib/models/QuestionModel.dart
@@ -0,0 +1,30 @@
+class Question {
+  String id;
+  String question;
+  String type;
+  String code;
+  String opt1;
+  String opt2;
+  String opt3;
+  String opt4;
+
+
+  Question({this.id, this.question, this.type, this.code, this.opt1, this.opt2,
+      this.opt3, this.opt4});
+
+  factory Question.fromJson(Map<String, dynamic> parsedJson)
+  {
+    return new Question(
+        id: parsedJson['id'],
+        question: parsedJson['ques'],
+        type: parsedJson['type'],
+        code: parsedJson['code'],
+        opt1: parsedJson['opt1'],
+        opt2: parsedJson['opt2'],
+        opt3: parsedJson['opt3'],
+        opt4: parsedJson['opt4']
+    );
+  }
+
+}
+
