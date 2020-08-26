ad79090a5b9907eabe944ef003efc57a73c1f2b4
Sumit <sumit.kr445@gmail.com>
2020-08-26 19:43:09 +0530

    added Student model

---
+++ lib/models/StudentModel.dart
@@ -0,0 +1,10 @@
+class Student{
+  String studentName;
+  String studentUSN;
+  String studentPhone;
+  String studentEmail;
+
+  Student(this.studentName, this.studentUSN, this.studentPhone,
+      this.studentEmail);
+
+}
