cba6f74c66a310f5911611f2b3c86443d01f1892
Sumit <sumit.kr445@gmail.com>
2020-09-05 01:37:00 +0530

    added student result model

---
+++ lib/models/StudentResultModel.dart
@@ -0,0 +1,12 @@
+class StudentResult{
+  String studentName;
+  String studentUSN;
+  String studentPhone;
+  String studentEmail;
+  String attemptedQuestions;
+  String score;
+
+  StudentResult(this.studentName, this.studentUSN, this.studentPhone,
+      this.studentEmail,this.attemptedQuestions,this.score);
+
+}
