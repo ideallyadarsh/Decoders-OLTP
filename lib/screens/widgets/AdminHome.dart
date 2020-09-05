f4b4defa710034f431df202d41627571be2fc137
Sumit <sumit.kr445@gmail.com>
2020-09-05 16:56:14 +0530

    added Admin Home

---
+++ lib/screens/widgets/AdminHome.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AdminHome extends StatefulWidget {
+  @override
+  _AdminHomeState createState() => _AdminHomeState();
+}
+
+class _AdminHomeState extends State<AdminHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
